5678f590a072fe33647d1bd693ed8091dbb92855
Artem Oleshkevich <oleshkevich.artem.1998@gmail.com>
2022-07-21 23:25:39 +0300

    Create a project

---
+++ Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Oleshkevich Artem on 21.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
